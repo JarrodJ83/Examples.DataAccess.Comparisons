36b7a463e4337f502b2578ec685cb091888feb7a
Jarrod Johnson <jjohnson@new-innov.com>
2018-08-13 11:42:56 -0400

    Inject GetAllProductsPaged request handler into CqrsProductsController

--- WebApi/Controllers/CQRSProductsController.cs
+++ WebApi/Controllers/CQRSProductsController.cs
@@ -1,39 +1,54 @@
 ﻿using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+using DomainModel;
 using Microsoft.AspNetCore.Mvc;
+using Requests;
 
 namespace WebApi.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class CQRSProductsController : ControllerBase
+    public class CqrsProductsController : ControllerBase
     {
-        // GET api/values
-        [HttpGet]
-        public ActionResult<IEnumerable<string>> Get()
+        private readonly IRequestHandler<Requests.GetAllProductsPaged, Product[]> _getAllProductsPaged;
+
+        public CqrsProductsController(IRequestHandler<Requests.GetAllProductsPaged, Product[]> getAllProductsPaged)
         {
-            return new string[] { "value1", "value2" };
+            _getAllProductsPaged = getAllProductsPaged;
         }
 
-        // GET api/values/5
+        // GET api/CQRSProducts
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Product>>> Get([FromQuery]int pageSize, [FromQuery]int offset)
+        {
+            return await _getAllProductsPaged.HandleAsync(new Requests.GetAllProductsPaged
+            {
+                Offset = offset,
+                PageSize = pageSize
+            });
+        }
+
+        // GET api/CQRSProducts/5
         [HttpGet("{id}")]
         public ActionResult<string> Get(int id)
         {
             return "value";
         }
 
-        // POST api/values
+        // POST api/CQRSProducts
         [HttpPost]
         public void Post([FromBody] string value)
         {
         }
 
-        // PUT api/values/5
+        // PUT api/CQRSProducts/5
         [HttpPut("{id}")]
         public void Put(int id, [FromBody] string value)
         {
         }
 
-        // DELETE api/values/5
+        // DELETE api/CQRSProducts/5
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
