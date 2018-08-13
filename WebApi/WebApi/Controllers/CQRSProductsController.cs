e7efb2eb4e0f6f779da2ac7eeb2bf6141ebd54d6
Jarrod Johnson <jjohnson@new-innov.com>
2018-08-13 11:17:16 -0400

    Change ValuesController to CQRSProductsController

--- WebApi/WebApi/Controllers/CQRSProductsController.cs
+++ WebApi/WebApi/Controllers/CQRSProductsController.cs
@@ -1,14 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
 
 namespace WebApi.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class ValuesController : ControllerBase
+    public class CQRSProductsController : ControllerBase
     {
         // GET api/values
         [HttpGet]
