5e2b49336d594affc567239a3e828756f0b940c0
Jarrod Johnson <jjohnson@new-innov.com>
2018-08-13 11:17:00 -0400

    Create domain model project

---
+++ DomainModel/ProductStore.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DomainModel
+{
+    class ProductStore
+    {
+        public List<Product> Products;
+
+        public ProductStore(int numberOfProducts)
+        {
+            Products = new List<Product>();
+            var rand = new Random();
+
+            var currentId = 1;
+            Enumerable.Range(0, numberOfProducts).ToList()
+                .ForEach(id =>
+                {
+                    var newProduct = new Product(currentId, LoremNET.Lorem.Words(1, 4, true), rand.NextDouble() * rand.Next(1, 1000));
+
+                    Products.Add(newProduct);
+
+                    currentId++;
+                });
+        }
+    }
+}
