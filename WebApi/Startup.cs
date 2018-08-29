a5af617d6d6ba7b473795c4f5d21400c9f065703
Jarrod Johnson <jjohnson@new-innov.com>
2018-08-29 19:00:06 -0400

    Remove unnecessary files for non-solid version

--- WebApi/Startup.cs
+++ WebApi/Startup.cs
@@ -1,31 +1,18 @@
-﻿using DomainModel;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Controllers;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Queries;
-using Repositories;
-using Repositories.Core;
-using Requests;
-using Services;
-using Services.Core;
 using SimpleInjector;
-using SimpleInjector.Integration.AspNetCore.Mvc;
-using SimpleInjector.Lifestyles;
 
 namespace WebApi
 {
     public class Startup
     {
         private Container _container;
-
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
-            _container = new Container();
-            _container.Options.DefaultScopedLifestyle = new AsyncScopedLifestyle();
         }
 
         public IConfiguration Configuration { get; }
@@ -33,16 +20,6 @@ namespace WebApi
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
-
-            _container.Options.DefaultScopedLifestyle = new AsyncScopedLifestyle();
-
-            RegisterServicesAndRepos(_container);
-            RegisterCQRSDependencies(_container);
-
-            services.AddSingleton<IControllerActivator>(new SimpleInjectorControllerActivator(_container));
-            
-            services.EnableSimpleInjectorCrossWiring(_container);
-            services.UseSimpleInjectorAspNetRequestScoping(_container);
         }
 
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
@@ -57,24 +34,6 @@ namespace WebApi
             }
 
             app.UseMvc();
-        }
-
-        private void RegisterServicesAndRepos(Container container)
-        {
-            //    container.Register(typeof(IRepository<>), new[] { typeof(ProductRespository).Assembly }, Lifestyle.Scoped);
-            //    container.Register<IProductRepository, ProductRespository>(Lifestyle.Scoped);
-            //    container.Register<IProductService, ProductService>(Lifestyle.Scoped);
-        }
-
-        private void RegisterCQRSDependencies(Container container)
-        {
-            container.Register(typeof(IRequestHandler<>), new[] { typeof(RequestHandlers.GetAllProductsPaged).Assembly }, Lifestyle.Scoped);
-            container.Register(typeof(IRequestHandler<,>), new[] { typeof(RequestHandlers.GetAllProductsPaged).Assembly }, Lifestyle.Scoped);
-            //container.Register(typeof(ICommandHandler<>), assemblies, Lifestyle.Scoped);
-            container.Register(typeof(IQueryHandler<,>), new[] { typeof(QueryHandlers.AllProductsPaged).Assembly }, Lifestyle.Scoped);
-            
-            container.Register<IEntityStore<Product>>(() => new ProductStore(100), Lifestyle.Singleton);
-
         }
     }
 }
