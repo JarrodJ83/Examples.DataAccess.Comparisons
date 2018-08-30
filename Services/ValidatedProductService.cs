29f825071b61240ca5454670169c08af01b7b46d
Jarrod Johnson <jjohnson@new-innov.com>
2018-08-30 12:30:15 -0400

    Add ValidatedProductService

---
+++ Services/ValidatedProductService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading.Tasks;
+using DomainModel;
+using Services.Core;
+
+namespace Services
+{
+    public class ValidatedProductService : IProductService
+    {
+        private readonly IProductService _productService;
+        private const int _maxPageSize = 100;
+
+        public ValidatedProductService(IProductService productService)
+        {
+            _productService = productService;
+        }
+        public async Task<PagedData<Product>> GetAllProductsPagedAsync(int offset, int pageSize)
+        {
+            if (pageSize > _maxPageSize)
+                throw new Exception($"PageSize must be equal or less than {_maxPageSize}");
+
+            return await _productService.GetAllProductsPagedAsync(offset, pageSize);
+        }
+
+        public async Task<Product> GetProductAsync(int productId)
+        {
+            return await _productService.GetProductAsync(productId);
+        }
+    }
+}
